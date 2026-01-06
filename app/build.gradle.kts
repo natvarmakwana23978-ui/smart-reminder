b3c60b50f03f0c1ee93ece132854bbeae46e6bb3
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-06 07:30:27 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,61 +1,61 @@
+
 plugins {
-    id 'com.android.application'
-    id 'org.jetbrains.kotlin.android'
-    id 'org.jetbrains.kotlin.kapt' // આ લાઈન ડેટાબેઝ માટે ખૂબ જરૂરી છે
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt") // આ લાઈન ડેટાબેઝ માટે જરૂરી છે
 }
 
 android {
-    namespace 'com.indian.calendar'
-    compileSdk 34
+    namespace = "com.indian.calendar"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId "com.indian.calendar"
-        minSdk 24
-        targetSdk 34
-        versionCode 1
-        versionName "1.0"
+        applicationId = "com.indian.calendar"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
 
-        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
-        release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = '1.8'
+        jvmTarget = "1.8"
     }
 }
 
 dependencies {
-    implementation 'androidx.core:core-ktx:1.12.0'
-    implementation 'androidx.appcompat:appcompat:1.6.1'
-    implementation 'com.google.android.material:material:1.11.0'
-    implementation 'androidx.constraintlayout:constraintlayout:2.1.4'
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // ૧. Google ML Kit (ભાષાંતર માટે)
-    implementation 'com.google.mlkit:translate:17.0.1'
+    implementation("com.google.mlkit:translate:17.0.1")
 
-    // ૨. OkHttp (ગૂગલ શીટ ડેટા લોડ કરવા માટે)
-    implementation 'com.squareup.okhttp3:okhttp:4.12.0'
+    // ૨. OkHttp (ગૂગલ શીટ માટે)
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
-    // ૩. Room Database (ડિજિટલ ડટ્ટા અને પર્સનલ નોટ્સ માટે)
-    def room_version = "2.6.1"
-    implementation "androidx.room:room-runtime:$room_version"
-    implementation "androidx.room:room-ktx:$room_version"
-    kapt "androidx.room:room-compiler:$room_version"
+    // ૩. Room Database (ડેટાબેઝ માટે)
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
 
-    // કોર્યુટિન્સ (બેકગ્રાઉન્ડ ટાસ્ક માટે)
-    implementation "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3"
+    // ૪. Coroutines (બેકગ્રાઉન્ડ ટાસ્ક માટે)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
-    testImplementation 'junit:junit:4.13.2'
-    androidTestImplementation 'androidx.test.ext:junit:1.1.5'
-    androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.1'
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
-
