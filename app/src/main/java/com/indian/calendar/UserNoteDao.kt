a63b019138123c6d1b8ca08abf2d287b5ccf1160
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-09 19:26:38 +0530

    Update UserNoteDao.kt

--- app/src/main/java/com/indian/calendar/UserNoteDao.kt
+++ app/src/main/java/com/indian/calendar/UserNoteDao.kt
@@ -4,20 +4,12 @@ import androidx.room.*
 
 @Dao
 interface UserNoteDao {
-    // કોઈ ચોક્કસ તારીખ માટે નોંધ મેળવવા માટે
-    @Query("SELECT * FROM user_notes WHERE dateKey = :date")
-    suspend fun getNoteByDate(date: String): UserNote?
-
-    // નવી નોંધ ઉમેરવા અથવા જૂની નોંધ અપડેટ કરવા માટે
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNote(note: UserNote)
 
-    // ૩૬૫ દિવસના લિસ્ટ માટે બધી જ નોંધો મેળવવા
     @Query("SELECT * FROM user_notes")
     suspend fun getAllNotes(): List<UserNote>
 
-    // કોઈ નોંધ ડીલીટ કરવા માટે (જો જરૂર પડે તો)
     @Delete
     suspend fun deleteNote(note: UserNote)
 }
-
