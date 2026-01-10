5587445ca57f0abe5a8ff86431c40200fb69d643
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-10 09:33:16 +0530

    Update CalendarSelectionActivity.kt

--- app/src/main/java/com/indian/calendar/CalendarSelectionActivity.kt
+++ app/src/main/java/com/indian/calendar/CalendarSelectionActivity.kt
@@ -18,7 +18,7 @@ class CalendarSelectionActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var progressBar: ProgressBar
     private val calendarList = mutableListOf<CalendarModel>()
-    private val webAppUrl = "તમારી_URL_અહીં"
+    private val webAppUrl = "https://script.google.com/macros/s/AKfycbw7U_En4xgEayUOV3N3HwoW-GkfyOONMUxidIeZbOAxCfGJqxcXeWFvz6pnJL5nRQQ3/exec"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
