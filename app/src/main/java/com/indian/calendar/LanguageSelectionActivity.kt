5aef739471a46a0583838b0591289aa32791fddb
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-10 18:41:54 +0530

    Update LanguageSelectionActivity.kt

--- app/src/main/java/com/indian/calendar/LanguageSelectionActivity.kt
+++ app/src/main/java/com/indian/calendar/LanguageSelectionActivity.kt
@@ -4,30 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LanguageSelectionActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        // સિમ્પલ લેઆઉટ પ્રોગ્રામેટિકલી બનાવીએ છીએ જેથી નવી XML ની ઝંઝટ ના રહે
         val listView = ListView(this)
-        val languages = arrayOf("ગુજરાતી", "Hindi", "English", "Marathi", "Tamil", "Telugu", "Kannada")
-        
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, languages)
-        listView.adapter = adapter
-
+        val languages = arrayOf("ગુજરાતી", "Hindi", "English", "Marathi", "Tamil")
+        listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, languages)
         setContentView(listView)
 
         listView.setOnItemClickListener { _, _, position, _ ->
-            val selectedLang = languages[position]
-            // ભાષા પસંદ કર્યા પછી મેઈન કેલેન્ડર (પગલું ૬) પર જશે
             val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("selected_language", selectedLang)
+            intent.putExtra("lang", languages[position])
             startActivity(intent)
         }
     }
 }
-
