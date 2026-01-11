fae05a27e5c037d45d594a10c9a99fd242c3d7ab
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-11 13:23:52 +0530

    Update UserNoteDao.kt

--- app/src/main/java/com/indian/calendar/UserNoteDao.kt
+++ app/src/main/java/com/indian/calendar/UserNoteDao.kt
@@ -1,18 +1,12 @@
 package com.indian.calendar
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 
 @Dao
 interface UserNoteDao {
     @Query("SELECT * FROM user_notes")
     fun getAllNotes(): List<UserNote>
 
-    // આ ફંક્શન વિજેટ માટે જરૂરી છે
     @Query("SELECT * FROM user_notes WHERE date = :date LIMIT 1")
     fun getNoteByDate(date: String): UserNote?
 
