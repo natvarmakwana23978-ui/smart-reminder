ac3ab0a12656e98553bc88c8a7c0368adde1666c
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-11 13:22:35 +0530

    Update ApiService.kt

--- app/src/main/java/com/indian/calendar/ApiService.kt
+++ app/src/main/java/com/indian/calendar/ApiService.kt
@@ -1,1 +1,10 @@
+package com.indian.calendar
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("macros/s/AKfycbxoURGUvXz4bVzQrfK5i4Db4BnJK35ab7vjYyNPhEBjDxBjGJg5afbR8MUQ3WMgXqQYOw/exec")
+    fun getCalendars(): Call<List<CalendarModel>>
+}
 
