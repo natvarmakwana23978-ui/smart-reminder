5df165643afd582e3428871d47ddfa1e5f14ff49
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-13 13:45:07 +0530

    Update CalendarDayData.kt

--- app/src/main/java/com/indian/calendar/CalendarDayData.kt
+++ app/src/main/java/com/indian/calendar/CalendarDayData.kt
@@ -1,1 +1,9 @@
+package com.indian.calendar
 
+import com.google.gson.annotations.SerializedName
+
+data class CalendarDayData(
+    @SerializedName("date") val date: String,      // શીટની તારીખ
+    @SerializedName("detail") val detail: String,  // પસંદ કરેલ કેલેન્ડરની વિગત (તિથિ)
+    @SerializedName("festival") val festival: String // તહેવારની વિગત
+)
