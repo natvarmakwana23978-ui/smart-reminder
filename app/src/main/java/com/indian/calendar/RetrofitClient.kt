c31f701f8cd7b1badb73dc76eb8dbba90cbec4a1
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-14 21:49:29 +0530

    Update RetrofitClient.kt

--- app/src/main/java/com/indian/calendar/RetrofitClient.kt
+++ app/src/main/java/com/indian/calendar/RetrofitClient.kt
@@ -1,18 +1,27 @@
 package com.indian.calendar
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-    // તમારી નવી લિંક
-    private const val BASE_URL = "https://script.google.com/macros/s/AKfycbyEBQpPWdz_7SjzjghU4IaMBlaR98LORop8qeGhBXmlfog028zg6TfdujX0RQzehUDH/"
+    private const val BASE_URL = "https://script.google.com/macros/s/AKfycbEBQpPWdz_7SjzjghU4IaMBIaR98LOrop8qeGhBXmlfog028zg6TfdujX0RQzehUDH/"
+
+    // ગૂગલના રીડાયરેક્ટ્સને હેન્ડલ કરવા માટે ક્લાયન્ટ
+    private val client = OkHttpClient.Builder()
+        .followRedirects(true)
+        .followSslRedirects(true)
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client) // આ ક્લાયન્ટ ઉમેરવો જરૂરી છે
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-        // અહીં ::class.java વાપરવું ફરજિયાત છે
         retrofit.create(ApiService::class.java)
     }
 }
