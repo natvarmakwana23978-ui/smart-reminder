364513f2312c1508a98cb4aa53ff40096b60ee22
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-15 16:03:23 +0530

    Update ApiService.kt

--- app/src/main/java/com/indian/calendar/ApiService.kt
+++ app/src/main/java/com/indian/calendar/ApiService.kt
@@ -6,18 +6,10 @@ import retrofit2.http.Query
 
 interface ApiService {
 
-    /**
-     * 1️⃣ બધા ઉપલબ્ધ કેલેન્ડર લિસ્ટ માટે
-     * (Google Sheet → App Script → JSON Array)
-     */
-    @GET("macros/s/AKfycbzsFfM_jo2P_PmCtDyccoC6KIubETZxjAnAtwLBTJRtidKIicS5cKf9l5KrMC9TDRWt/exec")
+    @GET("exec")
     fun getCalendars(): Call<List<CalendarModel>>
 
-    /**
-     * 2️⃣ યુઝર દ્વારા પસંદ કરેલ કેલેન્ડર મુજબનો ડેટા
-     * colIndex = Google Sheet નો column index
-     */
-    @GET("macros/s/AKfycbzsFfM_jo2P_PmCtDyccoC6KIubETZxjAnAtwLBTJRtidKIicS5cKf9l5KrMC9TDRWt/exec")
+    @GET("exec")
     fun getCalendarData(
         @Query("colIndex") colIndex: Int
     ): Call<List<CalendarDayData>>
