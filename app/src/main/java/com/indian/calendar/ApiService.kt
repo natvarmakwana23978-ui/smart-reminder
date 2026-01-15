f248129adda4db7d3c12e7de403df00839d0877f
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-15 07:23:33 +0530

    Update ApiService.kt

--- app/src/main/java/com/indian/calendar/ApiService.kt
+++ app/src/main/java/com/indian/calendar/ApiService.kt
@@ -3,12 +3,14 @@ package com.indian.calendar
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
+import retrofit2.http.Url
 
 interface ApiService {
-    // આખી લિંક અહીં જ આપી દીધી છે
-    @GET("macros/s/AKfycbEBQpPWdz_7SjzjghU4IaMBIaR98LOrop8qeGhBXmlfog028zg6TfdujX0RQzehUDH/exec")
+    // @Url વાપરવાથી આપણે ડાયરેક્ટ આખી લિંક મોકલી શકીશું
+    @GET("https://script.google.com/macros/s/AKfycbEBQpPWdz_7SjzjghU4IaMBIaR98LOrop8qeGhBXmlfog028zg6TfdujX0RQzehUDH/exec")
     fun getCalendars(): Call<List<CalendarModel>>
 
-    @GET("macros/s/AKfycbEBQpPWdz_7SjzjghU4IaMBIaR98LOrop8qeGhBXmlfog028zg6TfdujX0RQzehUDH/exec")
+    // બીજા ફંક્શનમાં પણ આખું URL જ વાપરીએ (ફક્ત colIndex પેરામીટર સાથે)
+    @GET("https://script.google.com/macros/s/AKfycbEBQpPWdz_7SjzjghU4IaMBIaR98LOrop8qeGhBXmlfog028zg6TfdujX0RQzehUDH/exec")
     fun getCalendarData(@Query("colIndex") colIndex: Int): Call<List<CalendarDayData>>
 }
