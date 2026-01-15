f2917599ae56de452b87c18de74151a405b8f053
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-15 10:50:34 +0530

    Update ApiService.kt

--- app/src/main/java/com/indian/calendar/ApiService.kt
+++ app/src/main/java/com/indian/calendar/ApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 
 interface ApiService {
 
-    // Calendar List (Gujarati, Hindi, etc.)
+    // Calendar List (Gujarati, Hindi, Islamic, etc.)
     @GET("exec")
     fun getCalendars(): Call<List<CalendarModel>>
 
