01951c733ba35cfb4cc9a838fc36818e1d8b6339
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-16 12:57:17 +0530

    Update CalendarDayData.kt

--- app/src/main/java/com/indian/calendar/CalendarDayData.kt
+++ app/src/main/java/com/indian/calendar/CalendarDayData.kt
@@ -5,6 +5,9 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class CalendarDayData(
-    val festival: String? = null,
-    val reminders: MutableList<Reminder> = mutableListOf()
+    val Date: String,
+    val Gujarati_Month: String,
+    val Tithi: String,
+    val Day: String,
+    val Festival_English: String
 ) : Parcelable
