8768c501125d6e182faac419c15073ef5bea6efa
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-19 11:07:43 +0530

    Update ApiService.kt

--- app/src/main/java/com/indian/calendar/ApiService.kt
+++ app/src/main/java/com/indian/calendar/ApiService.kt
@@ -1,16 +1,17 @@
 package com.indian.calendar
 
 import com.indian.calendar.model.CalendarDayData
+import com.indian.calendar.model.CalendarItem
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    // કેલેન્ડર લિસ્ટ મેળવવા માટે (Selection Activity માટે)
+    // કેલેન્ડરનું લિસ્ટ મેળવવા માટે
     @GET("exec")
     fun getCalendars(@Query("action") action: String = "getCalendars"): Call<List<CalendarItem>>
 
-    // જે-તે ભાષાનો ડેટા મેળવવા માટે (View Activity માટે)
+    // જે-તે ભાષાનો ડેટા (૨૬ ભાષા) મેળવવા માટે [cite: 2026-01-07]
     @GET("exec")
     fun getCalendarData(
         @Query("colIndex") colIndex: Int,
