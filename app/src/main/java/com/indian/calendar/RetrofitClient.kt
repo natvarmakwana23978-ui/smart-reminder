6ef0e5420e97dc165ba334e6127c5a7d0c0aca0a
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-20 13:47:00 +0530

    Update RetrofitClient.kt

--- app/src/main/java/com/indian/calendar/RetrofitClient.kt
+++ app/src/main/java/com/indian/calendar/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     // BASE_URL હંમેશા સ્લેશ (/) થી પૂરી થવી જોઈએ અને તેમાં 'exec' ન હોવું જોઈએ
-    private const val BASE_URL = "https://script.google.com/macros/s/AKfycbymdxMlklXy0H6dX5mHHEWoHInTsbFhtmDhTsDcEWoZmSF7mhH4uUsQJEqsiBGgOQEC/exec" 
+    private const val BASE_URL = "https://script.google.com/macros/s/AKfycbzHOrbkFzwuFut-nIKgodBGji_DLE0LQda3qeH5Ml0oVXdQdOg18tUG9CtmCX8nBYF7/exec" 
 
     val api: ApiService by lazy {
         Retrofit.Builder()
