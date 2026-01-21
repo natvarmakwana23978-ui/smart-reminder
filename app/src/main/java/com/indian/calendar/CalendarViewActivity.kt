c83b96abdce62a013be380f6324815b41f8a7349
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-21 19:18:25 +0530

    Update CalendarViewActivity.kt

--- app/src/main/java/com/indian/calendar/CalendarViewActivity.kt
+++ app/src/main/java/com/indian/calendar/CalendarViewActivity.kt
@@ -1,41 +1,9 @@
-package com.indian.calendar
-
-import android.os.Bundle
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
-class CalendarViewActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calendar_view)
-
-        recyclerView = findViewById(R.id.calendarRecyclerView)
-        // ૭ કોલમવાળી ગ્રીડ સેટ કરી
-        recyclerView.layoutManager = GridLayoutManager(this, 7)
-
-        val colIndex = intent.getIntExtra("COL_INDEX", 1)
-        loadCalendarData(colIndex)
-    }
-
-    private fun loadCalendarData(colIndex: Int) {
-        RetrofitClient.api.getCalendarData(colIndex = colIndex).enqueue(object : Callback<List<CalendarDayData>> {
+// ... બાકીનો કોડ ઉપર મુજબ ...
             override fun onResponse(call: Call<List<CalendarDayData>>, response: Response<List<CalendarDayData>>) {
+                progressBar.visibility = View.GONE
                 if (response.isSuccessful) {
-                    val days = response.body() ?: emptyList()
-                    // અહીં સુધારો કર્યો: CalendarViewAdapter ને બદલે CalendarDayAdapter વાપર્યું [cite: 2026-01-21]
-                    recyclerView.adapter = CalendarDayAdapter(days)
+                    val daysList = response.body() ?: emptyList()
+                    // અહીં નામ 'CalendarAdapter' હોવું જોઈએ [cite: 2026-01-21]
+                    recyclerView.adapter = CalendarAdapter(daysList) 
                 }
-            }
-            override fun onFailure(call: Call<List<CalendarDayData>>, t: Throwable) {
-                Toast.makeText(this@CalendarViewActivity, "ડેટા લોડ કરવામાં નિષ્ફળ", Toast.LENGTH_SHORT).show()
-            }
-        })
-    }
-}
+// ... બાકીનો કોડ ઉપર મુજબ ...
