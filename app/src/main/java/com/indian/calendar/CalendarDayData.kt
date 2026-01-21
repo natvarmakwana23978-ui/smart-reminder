1a00fb0242d171f1e542ff7c7a64c39ac768887c
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-21 17:53:11 +0530

    Update CalendarDayData.kt

--- app/src/main/java/com/indian/calendar/CalendarDayData.kt
+++ app/src/main/java/com/indian/calendar/CalendarDayData.kt
@@ -1,17 +1,14 @@
 package com.indian.calendar
 
-import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 data class CalendarDayData(
     @SerializedName("ENGLISH") 
-    val Date: String = "", // 'd' ને બદલે 'D' [cite: 2026-01-21]
+    val englishDate: String? = null,
     
     @SerializedName("local_date") 
-    val Gujarati: String = "", // 'localDate' ને બદલે 'Gujarati' [cite: 2026-01-21]
+    val localDate: String? = null,
     
     @SerializedName("Alert") 
-    val Alert: String = ""
-) : Parcelable
+    val alert: String? = null
+)
