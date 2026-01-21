b00df4a06cdb4cafe651052456f17e159fc4dc90
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-21 19:17:52 +0530

    Update ApiService.kt

--- app/src/main/java/com/indian/calendar/ApiService.kt
+++ app/src/main/java/com/indian/calendar/ApiService.kt
@@ -5,14 +5,14 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    // મુખ્ય કેલેન્ડર લિસ્ટ મેળવવા માટે [cite: 2026-01-20]
+    // મુખ્ય લિસ્ટ મેળવવા માટે [cite: 2026-01-21]
     @GET("exec")
-    fun getCalendars(): Call<List<CalendarItem>>
+    fun getCalendarList(): Call<List<CalendarItem>>
 
-    // કેલેન્ડરની અંદરનો ડેટા (તારીખ-તિથિ) મેળવવા માટે [cite: 2026-01-20]
+    // કેલેન્ડરના દિવસો મેળવવા માટે [cite: 2026-01-21]
     @GET("exec")
     fun getCalendarData(
-        @Query("action") action: String = "getCalendarDays", // સ્ક્રિપ્ટ મુજબનું સાચું નામ [cite: 2026-01-20]
+        @Query("action") action: String = "getCalendarDays",
         @Query("colIndex") colIndex: Int
     ): Call<List<CalendarDayData>>
 }
