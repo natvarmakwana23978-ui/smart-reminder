82ea84043404c11fbc942ad70f4caa58448346e1
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-01-22 21:52:42 +0530

    Update CalendarDayData.kt

--- app/src/main/java/com/indian/calendar/CalendarDayData.kt
+++ app/src/main/java/com/indian/calendar/CalendarDayData.kt
@@ -4,6 +4,5 @@ import com.google.gson.JsonObject
 
 data class CalendarDayData(
     val englishDate: String,  // કોલમ A (ENGLISH)
-    val localDate: String?,   // કોલમ B (ગુજરાતી (Gujarati))
-    val allData: JsonObject   // આખી રો નો ડેટા (બધી ૨૭ કોલમ્સ માટે)
+    val allData: JsonObject   // આખા ૨૭ કોલમનો ડેટા (કોલમ B થી AA સુધી બધું જ) [cite: 2026-01-22]
 )
