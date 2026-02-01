805fa419322663fe0a0626a0220ccab146356274
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-02-01 13:46:31 +0530

    Update MonthFragment.kt

--- app/src/main/java/com/indian/calendar/MonthFragment.kt
+++ app/src/main/java/com/indian/calendar/MonthFragment.kt
@@ -1,1 +1,43 @@
+package com.indian.calendar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import com.google.gson.reflect.TypeToken
+
+class MonthFragment : Fragment() {
+
+    companion object {
+        fun newInstance(monthData: List<JsonObject>, lang: String): MonthFragment {
+            val fragment = MonthFragment()
+            val args = Bundle()
+            args.putString("MONTH_DATA", Gson().toJson(monthData))
+            args.putString("LANG", lang)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_month, container, false)
+        val rv = view.findViewById<RecyclerView>(R.id.rvMonthCalendar)
+        
+        val jsonData = arguments?.getString("MONTH_DATA")
+        val lang = arguments?.getString("LANG") ?: "ગુજરાતી (Gujarati)"
+        
+        val dataList: List<JsonObject> = Gson().fromJson(jsonData, object : TypeToken<List<JsonObject>>() {}.type)
+        val finalItems = dataList.map { CalendarDayData(it.get("ENGLISH").asString, it) }
+
+        rv.layoutManager = GridLayoutManager(context, 7)
+        rv.adapter = CalendarAdapter(finalItems, lang)
+        
+        return view
+    }
+}
 
