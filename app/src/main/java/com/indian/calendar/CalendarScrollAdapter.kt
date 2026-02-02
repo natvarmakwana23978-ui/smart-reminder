23b844ff1b8166b78527b45470d4c3b7ce3226d0
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-02-02 20:03:27 +0530

    Update CalendarScrollAdapter.kt

--- app/src/main/java/com/indian/calendar/CalendarScrollAdapter.kt
+++ app/src/main/java/com/indian/calendar/CalendarScrollAdapter.kt
@@ -1,1 +1,15 @@
+// ... (જૂનો કોડ) ...
+
+fun bind(monthPos: Int, jsonData: String, lang: String) {
+    // સેમ્પલ ડેટા (અહીં તમારે ગૂગલ શીટનો ડેટા મેપ કરવાનો રહેશે)
+    val dayList = mutableListOf<DayModel>()
+    
+    // અત્યારે આપણે ટેસ્ટ કરવા માટે ૧ થી ૩૦ તારીખ ભરીએ
+    for (i in 1..30) {
+        dayList.add(DayModel(i.toString(), "તિથિ $i"))
+    }
+
+    daysRecyclerView.layoutManager = GridLayoutManager(itemView.context, 7)
+    daysRecyclerView.adapter = DaysAdapter(dayList)
+}
 
