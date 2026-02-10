3f66ad2a2b66ec8a6d250b772f8b0a5575ae6fe9
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2026-02-10 19:39:53 +0530

    Update CalendarSelectionActivity.kt

--- app/src/main/java/com/indian/calendar/CalendarSelectionActivity.kt
+++ app/src/main/java/com/indian/calendar/CalendarSelectionActivity.kt
@@ -1,51 +1,23 @@
 package com.smart.reminder
-
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-// સાચો બાઈન્ડિંગ પાથ અહીં છે
 import com.smart.reminder.databinding.ActivityCalendarSelectionBinding
 
 class CalendarSelectionActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityCalendarSelectionBinding
-
-    private val calendars = arrayOf(
-        "Vikram Samvat 2082", 
-        "Shaka Samvat 1947", 
-        "Hijri 1447", 
-        "Banswara Calendar", 
-        "Marathi Calendar"
-        // બાકીના નામ અહીં ઉમેરી શકાય
-    )
-
-    private val languages = arrayOf("Gujarati", "Hindi", "English", "Marathi")
+    private val calendars = arrayOf("ENGLISH", "ગુજરાતી (Gujarati)", "हिन्दी (Hindi)", "ઇસ્લામિક (Islamic)", "తెలుగు/ಕನ್ನಡ (Telugu/Kannada)", "தமிழ் (Tamil)", "മലയാളം (Malayalam)", "ਪੰਜਾਬੀ (Punjabi)", "ଓଡ଼િଆ (Odia)", "বাংলা (Bengali)", "नेपाली (Nepali)", "中文 (Chinese)", "עברית (Hebrew)", "فارસી (Persian)", "ኢትዮጵያ (Ethiopian)", "Basa Bali (Balinese)", "한국어 (Korean)", "Tiếng Việt (Vietnamese)", "ไทย (Thai)", "Français (French)", "မြန်မာဘာသာ (Burmese)", "کاشمیری (Kashmiri)", "મારવાડી (Marwari)", "日本語 (Japanese)", "অસમীયા (Assamese)", "سنڌી (Sindhi)", "བོད་སྐદ (Tibetan)")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        // View Binding સેટઅપ
         binding = ActivityCalendarSelectionBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // કેલેન્ડર સ્પિનર સેટઅપ
-        val calAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, calendars)
-        binding.calendarSpinner.adapter = calAdapter
-
-        // ભાષા સ્પિનર સેટઅપ
-        val langAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, languages)
-        binding.languageSpinner.adapter = langAdapter
-
-        // બટન ક્લિક ઇવેન્ટ
+        binding.calendarSpinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, calendars)
         binding.btnOpenCalendar.setOnClickListener {
-            val selectedCal = binding.calendarSpinner.selectedItem.toString()
-            val selectedLang = binding.languageSpinner.selectedItem.toString()
-
             val intent = Intent(this, CalendarViewActivity::class.java)
-            // ડેટા ટાઇપ સ્પષ્ટ કરવા માટે 'as String' ઉમેર્યું છે
-            intent.putExtra("SELECTED_CALENDAR", selectedCal as String)
-            intent.putExtra("SELECTED_LANG", selectedLang as String)
+            intent.putExtra("LANG_INDEX", binding.calendarSpinner.selectedItemPosition)
+            intent.putExtra("LANG_NAME", calendars[binding.calendarSpinner.selectedItemPosition])
             startActivity(intent)
         }
     }
